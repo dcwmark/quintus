27d3c321c5c88d2f7e85c1fb77d888e0b2ecaaff
Entaro Adun <dcwmark@gmail.com>
2024-09-16 20:27:52 -0400

    Inremental commit

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,38 +2,18 @@
 
 'use strict';
 
-import { API_KEYS } from "../constants/apiKeys";
-import { API_URLS } from "../constants/apiUrls";
 import axios from "axios";
 import 'dotenv/config';
+
+import { API_KEYS } from "../constants/apiKeys";
+import { API_URLS } from "../constants/apiUrls";
 
 const mistralApiKey = API_KEYS.MISTRAL_AI_API_KEY;
 const mistralApiUrl = API_URLS.MISTRAL_AI_API_URL;
 console.debug(`const mistralApiKey:: ${mistralApiKey}`);
 console.debug(`const mistralApiUrl:: ${mistralApiUrl}`);
-console.debug(`env mistralApiKey:: ${process.env.mistralApiKey}`);
-console.debug(`env mistralApiUrl:: ${process.env.mistralApiUrl}`);
-
-const generateText = async (prompt: string): Promise<string | null> => {
-  try {
-    const response = await axios.post(`${process.env.mistralApiUrl}/generate`, {
-      prompt: prompt,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.mistralApiKey}`
-      }
-    });
-
-    return response.data.text;
-  } catch (error) {
-    console.error(`Error generating text:: `, error);
-    return null;
-  }
-};
-
-const prompt = "Write a sonnet about a robot who dreams of becoming a gardener.";
-generateText(prompt)
-  .then(text => console.log(text))
+console.debug(`env mistralApiKey:: ${process.env[mistralApiKey]}`);
+console.debug(`env mistralApiUrl:: ${process.env[mistralApiUrl]}`);
 
 
 
