c566911f5eac69d81f486c5da8fb1a285ed025bd
Entaro Adun <dcwmark@gmail.com>
2024-10-28 21:16:24 -0400

    corrected typo

--- src/server/diagnostics.ts
+++ src/server/diagnostics.ts
@@ -2,7 +2,7 @@
 
 'use strict';
 
-import { geminiAIChat } from "./connections/geminiAIConnect";
+import { geminiAIChat, geminiAIEmbeddung } from "./connections/geminiAIConnect";
 import { mistralAIChat, mistralAIEmbeddung } from "./connections/mistralAIConnect";
 import { openAIChat, openAIEmbeddung } from "./connections/openAIConnect";
 
@@ -13,17 +13,21 @@ geminiAIChat(prompt)
   .then((response) => console.log(`geminiai response::`, response))
   .catch((error) => console.error(error));
 
-// mistralAIChat(prompt)
-//   .then((response) => console.log(`mistralai response::`, response.choices))
+// geminiAIEmbeddung(inputs)
+//   .then((response) => console.log(response))
 //   .catch((error) => console.error(error));
+
+mistralAIChat(prompt)
+  .then((response) => console.log(`mistralai response::`, response.choices))
+  .catch((error) => console.error(error));
 
 // mistralAIEmbeddung(inputs)
 //   .then((response) => console.log(response))
 //   .catch((error) => console.error(error));
 
-// openAIChat(prompt)
-//   .then((response) => console.log(`openai response::`, response.choices))
-//   .catch((error) => console.error(error));
+openAIChat(prompt)
+  .then((response) => console.log(`openai response::`, response.choices))
+  .catch((error) => console.error(error));
 
 // openAIEmbeddung(inputs)
 //   .then((response) => console.log(response))
