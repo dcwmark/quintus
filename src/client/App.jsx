fa97cc3b5fd495c1b600afbdb9d866e965ae297e
Entaro Adun <dcwmark@gmail.com>
2026-01-12 13:47:13 -0500

    main App jsx css

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import "./App.css";
+
 export default function App() {
   return (
     <div className="App">
--- src/client/main.jsx
+++ src/client/main.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Assuming you have an App component
 
+import "./main.css"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
