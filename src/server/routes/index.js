abfc9455f52fba270a9ecd33402b9d8b45aba436
Entaro Adun <dcwmark@gmail.com>
2026-01-15 01:02:42 -0500

    Starting Back End

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,12 +3,8 @@
 'use strict';
 
 import express from 'express';
-import multer from 'multer';
-import { InferenceClient } from '@huggingface/inference';
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
 
-const hf = new InferenceClient(process.env.HUGGINGFACE_API_KEY);
 
 export default router;
